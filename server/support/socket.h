c14868edd8eb59c780d07821d52a12d6cef94d75
tom08 <kroll001@live.missouristate.edu>
2017-01-29 10:51:45 -0600

    Implemented simple dir structure, started socket class

---
+++ server/support/socket.h
@@ -0,0 +1,16 @@
+//Created: 01/29/17
+//Created By: Tom
+
+//include guard
+#ifndef __SOCKET_H_INCLUDED__
+#define __SOCKET_H_INCLUDED__
+
+//Includes
+#include <sys/socket.h>
+
+class Socket {
+    public:
+    private:
+};
+
+#endif
